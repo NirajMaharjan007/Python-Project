e8678c4092782d365cb05b16a45ed160a8da1156
niraj.mhrjn770 <niraj.mhrjn770@gmail.com>
2023-03-21 01:01:21 +0545

    updated and commited

--- data/csvFile.py
+++ data/csvFile.py
@@ -1,7 +1,12 @@
 import csv
 
 
-def write_into_csv(emp_id=int, emp_name=str):
-    with open('./resources/file_data.csv', 'a', newline='\n') as file:
-        writer = csv.writer(file)
-        writer.writerow([emp_id, emp_name])
+class WriteFile:
+    def __init__(self):
+        print("tested")
+
+    def write_into_csv(self,emp_id=int, emp_name=str):
+        with open('./resources/file_data.csv', 'a', newline='\n') as file:
+            writer = csv.w
+            writer.writerow([emp_id, emp_name])
+            
