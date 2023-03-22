b074c6ae4862e189d3bdfbd10b8d1661fbd98236
niraj.mhrjn770 <niraj.mhrjn770@gmail.com>
2023-03-22 22:21:58 +0545

    Updated

--- data/csvFile.py
+++ data/csvFile.py
@@ -1,22 +1,31 @@
 import csv
+import pandas as pd
+from random import randint
 
 filename = './resources/file_data.csv'
 
 
 class WriteFile:
-    def __init__(self):
+    def __init__(self) -> None:
         print("tested")
 
-    def write_into_csv(self, emp_id=int, emp_name=str):
+    @staticmethod
+    def write_into_csv(emp_id=int, emp_name=str):
         with open(filename, 'a', newline='\n') as file:
             writer = csv.writer(file)
             writer.writerow([emp_id, emp_name])
 
 
 class Reader:
-    def __init__(self) -> None:
+    def __init__(self,   emp_id=int, emp_name=str) -> None:
         print("Reader")
+        self.emp_id = emp_id
+        self.emp_name = emp_name
 
-    def read_csv(self,  emp_id=int, emp_name=str):
-        with open(filename, 'r') as f:
-            reader = csv.reader(f)
+    def read(self):
+        df = pd.read_csv(filename)
+        print(df)
+
+
+reader = Reader(182, "niraj")
+reader.read()
